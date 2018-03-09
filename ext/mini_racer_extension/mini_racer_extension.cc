08946e4f37e7b41049e9e1850ffc9bcccf65281d
Jb Aviat <jb@sqreen.io>
2018-03-09 20:25:00 +0100

    Release GVL when calling a function

--- ext/mini_racer_extension/mini_racer_extension.cc
+++ ext/mini_racer_extension/mini_racer_extension.cc
@@ -56,6 +56,15 @@ typedef struct {
     size_t max_memory;
 } EvalParams;
 
+typedef struct {
+    ContextInfo *context_info;
+    char *function_name;
+    int argc;
+    Local<Function> fun;
+    Local<Value> *argv;
+    Local<Value> result;
+} FunctionCall;
+
 enum IsolateFlags {
     IN_GVL,
     DO_TERMINATE,
@@ -1079,42 +1088,40 @@ rb_context_dispose(VALUE self) {
     return Qnil;
 }
 
-#if 0
-static VALUE rb_function_get(VALUE self, VALUE function_name) {
-    ContextInfo* context_info;
-    Data_Get_Struct(self, ContextInfo, context_info);
+static void*
+nogvl_function_call(void *args) {
 
-    if (TYPE(function_name) != T_STRING) {
-        rb_raise(rb_eTypeError, "function_name should be a String");
+    FunctionCall *call = (FunctionCall *) args;
+    if (!call) {
+        return NULL;
     }
-
-    char *fname = RSTRING_PTR(function_name);
-    if (!fname) {
-        return Qnil;
-    }
-
-    Isolate* isolate = context_info->isolate_info->isolate;
-
-    Locker lock(isolate);
+    Isolate* isolate = call->context_info->isolate_info->isolate;
     Isolate::Scope isolate_scope(isolate);
-    HandleScope handle_scope(isolate);
+    EscapableHandleScope handle_scope(isolate);
     TryCatch trycatch(isolate);
-    Local<Context> context = context_info->context->Get(isolate);
+    Local<Context> context = call->context_info->context->Get(isolate);
     Context::Scope context_scope(context);
 
-    Local<Function> fun = v8::Local<v8::Function>::Cast(context->Global()->Get(
-            String::NewFromUtf8(isolate, fname)));
-    
-    return convert_v8_to_ruby(isolate, res);
+    Local<Function> fun = call->fun;
+
+    Local<v8::Value> res = fun->Call(context, context->Global(), call->argc, call->argv).ToLocalChecked();
+
+    call->result = handle_scope.Escape(res);
+
+    return NULL;
 }
-#endif 
+
+static void unblock_function(void *args) {
+    FunctionCall *call = (FunctionCall *) args;
+    call->context_info->isolate_info->interrupted = true;
+}
 
 static VALUE
-// rb_function_call(VALUE self, VALUE function_name, VALUE *args) {
 rb_function_call(int argc, VALUE *argv, VALUE self) {
 
     ContextInfo* context_info;
     Data_Get_Struct(self, ContextInfo, context_info);
+    VALUE res;
 
     if (argc < 1) {
         rb_raise(rb_eArgError, "need at least one argument %d", argc);
@@ -1130,40 +1137,52 @@ rb_function_call(int argc, VALUE *argv, VALUE self) {
         return Qnil;
     }
 
-    Isolate* isolate = context_info->isolate_info->isolate;
+    FunctionCall call;
 
-    Locker lock(isolate);
-    Isolate::Scope isolate_scope(isolate);
-    HandleScope handle_scope(isolate);
-    TryCatch trycatch(isolate);
-    Local<Context> context = context_info->context->Get(isolate);
-    Context::Scope context_scope(context);
-
-    // examples of such usage can be found in 
-    // https://github.com/v8/v8/blob/36b32aa28db5e993312f4588d60aad5c8330c8a5/test/cctest/test-api.cc#L15711
-
-    Local<Function> fun = v8::Local<v8::Function>::Cast(context->Global()->Get(
-            String::NewFromUtf8(isolate, fname)));
-    
-    int fun_argc = argc - 1;
-
-    v8::Local<Value> *fun_args = NULL;
-
-    if (fun_argc > 0) {
-        fun_args = (v8::Local<Value> *) malloc(sizeof(void *) * fun_argc);
-        if (!fun_args) {
-            return Qnil;
-        }
-        for(int i=0; i < fun_argc; i++) {
-            fun_args[i] = convert_ruby_to_v8(isolate, argv[i+1]);
-        }
+    call.function_name = fname;
+    call.context_info = context_info;
+    call.argc = argc - 1;
+    VALUE *call_argv = NULL;
+    if (call.argc > 0) {
+        // skip first argument which is the function name
+        call_argv = argv + 1;
     }
 
-    Local<v8::Value> res = fun->Call(context, context->Global(), argc - 1, fun_args).ToLocalChecked();
+    Isolate* isolate = context_info->isolate_info->isolate;
+    {
+        Locker lock(isolate);
+        Isolate::Scope isolate_scope(isolate);
+        HandleScope handle_scope(isolate);
+        TryCatch trycatch(isolate);
+        Local<Context> context = context_info->context->Get(isolate);
+        Context::Scope context_scope(context);
 
-    free(fun_args);
-        
-    return convert_v8_to_ruby(isolate, res);
+        // examples of such usage can be found in
+        // https://github.com/v8/v8/blob/36b32aa28db5e993312f4588d60aad5c8330c8a5/test/cctest/test-api.cc#L15711
+
+        Local<v8::Function> fun = Local<v8::Function>::Cast(context->Global()->Get(
+                        String::NewFromUtf8(isolate, call.function_name)));
+        call.fun = fun;
+        int fun_argc = call.argc;
+
+        if (fun_argc > 0) {
+            call.argv = (v8::Local<Value> *) malloc(sizeof(void *) * fun_argc);
+            if (!call.argv) {
+                return Qnil;
+            }
+            for(int i=0; i < fun_argc; i++) {
+                call.argv[i] = convert_ruby_to_v8(isolate, call_argv[i]);
+            }
+        }
+
+        rb_thread_call_without_gvl(nogvl_function_call, &call, unblock_function, &call);
+
+        free(call.argv);
+
+        // TODO - check for call errors
+        res = convert_v8_to_ruby(isolate, call.result);
+    }
+    return res;
 }
 
 extern "C" {
