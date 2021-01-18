0c447a12156e71e4606e53cc62d20927307b4cbb
Loic Nageleisen <loic.nageleisen@gmail.com>
2021-01-18 14:50:33 +0100

    Check for missing symbols at link time

--- ext/mini_racer_extension/extconf.rb
+++ ext/mini_racer_extension/extconf.rb
@@ -19,6 +19,8 @@ $CPPFLAGS += " -fvisibility=hidden "
 $CPPFLAGS += " -Wno-reserved-user-defined-literal" if IS_DARWIN
 
 $LDFLAGS.insert(0, " -stdlib=libc++ ") if IS_DARWIN
+$LDFLAGS += " -Wl,--no-undefined " unless IS_DARWIN
+$LDFLAGS += " -Wl,-undefined,error " if IS_DARWIN
 
 if ENV['CXX']
   puts "SETTING CXX"
