e82bd1684e3333924c7e413af10e03a96ec1d4ef
Sam Saffron <sam.saffron@gmail.com>
2024-09-09 08:35:38 +1000

    version bump

--- lib/mini_racer/version.rb
+++ lib/mini_racer/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module MiniRacer
-  VERSION = "0.17.0.pre"
-  LIBV8_NODE_VERSION = "~> 22.7.0.2"
+  VERSION = "0.17.0.pre2"
+  LIBV8_NODE_VERSION = "~> 22.7.0.3"
 end
